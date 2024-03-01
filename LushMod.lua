05667af299f34672d42b1c61533984529e18e896
Charlie Coleman <admin@luscious.dev>
2024-03-01 00:06:17 -0600

    add 777 card

--- LushMod.lua
+++ LushMod.lua
@@ -15,15 +15,16 @@ end
 
 function SMODS.INIT.LushMod()
     local lushJokers = {
-        j_jokester  = { order = 1, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 3, cost = 8, name = "The Jokester",  set = "Joker", config = { extra = 0.2, Xmult = 1 }, pos = { x = 0, y = 16 } },
-        j_brethren  = { order = 2, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 3, cost = 8, name = "The Brethren",  set = "Joker", config = { extra = 2 }, pos = { x = 1, y = 16, } },
-        j_trickster = { order = 3, unlocked = true, discovered = true, blueprint_compat = false, eternal_compat = true, rarity = 3, cost = 8, name = "The Trickster", set = "Joker", config = { extra = {}, }, pos = { x = 2, y = 16, }, },
-        j_top5      = { order = 4, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 2, cost = 6, name = "Top 5",         set = "Joker", config = { extra = { chips = 4, chip_mod = 2 } }, pos = { x = 3, y = 16 } },
-        j_buffer    = { order = 5, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 4, name = "The Buffer",    set = "Joker", config = { extra = { mult = 15 } }, pos = { x = 4, y = 16 } },
-        j_amazin    = { order = 6, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 4, name = "Amazin' Joker", set = "Joker", config = { extra = { chips = 25 } }, pos = { x = 5, y = 16 } },
-        j_hue       = { order = 7, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 2, cost = 6, name = "Hue Graph",     set = "Joker", config = { extra = 3 }, pos = { x = 6, y = 16 } },
-        j_wizard    = { order = 8, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 5, name = "Cloud Wizard",  set = "Joker", config = { extra = 1 }, pos = { x = 7, y = 16 } },
-        j_timely    = { order = 9, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 2, cost = 6, name = "Timely Lion",   set = "Joker", config = { extra = { mult = 12 } }, pos = { x = 8, y = 16 } },
+        j_jokester  = { order = 1,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 3, cost = 8, name = "The Jokester",      set = "Joker", config = { extra = 0.2, Xmult = 1 },              pos = { x = 0, y = 16 } },
+        j_brethren  = { order = 2,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 3, cost = 8, name = "The Brethren",      set = "Joker", config = { extra = 2 },                           pos = { x = 1, y = 16, } },
+        j_trickster = { order = 3,  unlocked = true, discovered = true, blueprint_compat = false, eternal_compat = true, rarity = 3, cost = 8, name = "The Trickster",     set = "Joker", config = { extra = {}, },                         pos = { x = 2, y = 16, }, },
+        j_top5      = { order = 4,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 2, cost = 6, name = "Top 5",             set = "Joker", config = { extra = { chips = 4, chip_mod = 2 } }, pos = { x = 3, y = 16 } },
+        j_buffer    = { order = 5,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 4, name = "The Buffer",        set = "Joker", config = { extra = { mult = 15 } },               pos = { x = 4, y = 16 } },
+        j_amazin    = { order = 6,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 4, name = "Amazin' Joker",     set = "Joker", config = { extra = { chips = 25 } },              pos = { x = 5, y = 16 } },
+        j_hue       = { order = 7,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 2, cost = 6, name = "Hue Graph",         set = "Joker", config = { extra = 3 },                           pos = { x = 6, y = 16 } },
+        j_wizard    = { order = 8,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 5, name = "Cloud Wizard",      set = "Joker", config = { extra = 1 },                           pos = { x = 7, y = 16 } },
+        j_timely    = { order = 9,  unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 2, cost = 6, name = "Timely Lion",       set = "Joker", config = { extra = { mult = 12 } },               pos = { x = 8, y = 16 } },
+        j_seven     = { order = 10, unlocked = true, discovered = true, blueprint_compat = true,  eternal_compat = true, rarity = 1, cost = 4, name = "Seven Seven Seven", set = "Joker", config = { extra = 3 },                           pos = { x = 9, y = 16 } },
     }
 
     -- Localization
@@ -66,6 +67,14 @@ function SMODS.INIT.LushMod()
         j_timely = {
           name = "Timely Lion",
           text = { "{C:mult}+#1#{} Mult on {C:attention}first{}", "and {C:attention}final{} hand", "of round." }
+        },
+        j_seven = {
+          name = "Seven Seven Seven",
+          text = {
+            "Create a {C:attention}random 7{}",
+            "if played hand contains",
+            "{C:attention}#1#{} or more {C:attention}7s{}",
+          }
         },
     }
 
@@ -261,6 +270,53 @@ function Card.calculate_joker(self, context)
                             }
                         end
                     end
+                    if self.ability.name == "Seven Seven Seven" then
+                      local sevens = 0
+                      for i = 1, #context.scoring_hand do
+                        if context.scoring_hand[i]:get_id() == 7 then
+                          sevens = sevens + 1
+                        end
+                      end
+                      if sevens >= 3 then
+                        G.E_MANAGER:add_event(Event({
+                          trigger = "after",
+                          delay = 0.7,
+                          func = function()
+                            local card = nil
+                            local newcards = {}
+                            local _rank = '7'
+                            local _suit = pseudorandom_element({'S','H','D','C'}, pseudoseed('777_create'))
+                            local cen_pool = {}
+                            local enhanced_poll = pseudorandom(pseudoseed('777enh'..G.GAME.round_resets.ante))
+                            if enhanced_poll > 0.7 then
+                              for k, v in pairs(G.P_CENTER_POOLS["Enhanced"]) do
+                                  if v.key ~= 'm_stone' then 
+                                      cen_pool[#cen_pool+1] = v
+                                  end
+                              end
+                              card = create_playing_card({front = G.P_CARDS[_suit..'_'.._rank], center = pseudorandom_element(cen_pool, pseudoseed('777_card'))}, G.hand, nil, false, {G.C.SECONDARY_SET.Spectral})
+                            else
+                              card = create_playing_card({front = G.P_CARDS[_suit..'_'.._rank], center = G.P_CENTERS.c_base}, G.hand, nil, false, {G.C.SECONDARY_SET.Spectral})
+                            end
+                            local edition_rate = 2
+                            local edition = poll_edition('777_edition'..G.GAME.round_resets.ante, edition_rate, true)
+                            card:set_edition(edition)
+                            local seal_rate = 10
+                            local seal_poll = pseudorandom(pseudoseed('777seal'..G.GAME.round_resets.ante))
+                            if seal_poll > 1 - 0.02*seal_rate then
+                                local seal_type = pseudorandom(pseudoseed('777sealtype'..G.GAME.round_resets.ante))
+                                if seal_type > 0.75 then card:set_seal('Red')
+                                elseif seal_type > 0.5 then card:set_seal('Blue')
+                                elseif seal_type > 0.25 then card:set_seal('Gold')
+                                else card:set_seal('Purple')
+                                end
+                            end
+                            newcards[1] = card
+                            playing_card_joker_effects(newcards)
+                            return true
+                          end}))
+                      end
+                    end
                 elseif context.after then
                   if context.cardarea == G.jokers then
                     if self.ability.name == "The Brethren" then
@@ -428,6 +484,9 @@ function Card.generate_UIBox_ability_table(self)
             customJoker = true
         elseif self.ability.name == 'Timely Lion' then
             loc_vars = {self.ability.extra.mult}
+            customJoker = true
+        elseif self.ability.name == 'Seven Seven Seven' then
+            loc_vars = {self.ability.extra}
             customJoker = true
         end
 
